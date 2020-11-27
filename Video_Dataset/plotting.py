32ecc6c8378cacb3f4eb26759a5ae7cfe5e680e0
guille921209 <51478653+guille921209@users.noreply.github.com>
2020-11-27 16:45:50 +0100

    Delete plotting.py

--- Video_Dataset/plotting.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def plot(embedder):
-
-    fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(15, 14))
-    ax[1].plot(embedder[0, :], embedder[1, :])
-
-    return
