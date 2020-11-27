5670f1f19f3f977c52be079e736e7007f6081422
guille921209 <51478653+guille921209@users.noreply.github.com>
2020-11-27 16:07:00 +0100

    Update complex_wtsquare.m
    In this function, we are going to split the complexes which form every cell to see the features which born and died in the same square on the complex.

--- Video_Dataset/complex_wtsquare.m
+++ Video_Dataset/complex_wtsquare.m
@@ -3,7 +3,7 @@ function [index_cuad, complejo]= complex_wtsquare(complejo)
 %with this function we are going to split the complexes which form 
 %every square to see the features which born and died in the same
 %square on complex, later the components which survived get the index where
-%square are. 
+%square are
 
 
 index_cuad=[];
